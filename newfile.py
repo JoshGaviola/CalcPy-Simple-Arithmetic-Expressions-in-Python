4b9a368d8b05202b12256ff20e21c69339989961
JoshGaviola <94226020+JoshGaviola@users.noreply.github.com>
2023-11-06 15:53:29 +0800

    Split the code into functions

--- newfile.py
+++ newfile.py
@@ -1,26 +1,46 @@
-num1 = 0.0
-num2 = 0.0
-operator = ""
-result = 0.0
+# Error handling
+def get_number_input(prompt):
+    while True:
+        try:
+            number = float(input(prompt))
+            return number
+        except ValueError:
+            print("Invalid input. Please enter a valid number.")
 
-num1 = float(input("Enter the first number: "))
+# Choosing arithmetic operations
+def get_operator_input():
+    valid_operators = ['+', '-', '*', '/']
+    while True:
+        operator = input("Choose an operator (+, -, *, /): ")
+        if operator in valid_operators:
+            return operator
+        else:
+            print("Invalid operator. Please choose +, -, *, or /.")
 
-operator = input("Choose an operator (+ for addition, - for subtraction, * for multiplication, / for division): ")
+# Calculate functions 
+def calculate(num1, operator, num2):
+    if operator == "+":
+        return num1 + num2
+    elif operator == "-":
+        return num1 - num2
+    elif operator == "*":
+        return num1 * num2
+    elif operator == "/":
+        if num2 == 0:
+            raise ZeroDivisionError("Error: Division by zero is not allowed.")
+        return num1 / num2
 
-num2 = float(input("Enter the second number: "))
-
-if operator == "+":
-    result = num1 + num2
-elif operator == "-":
-    result = num1 - num2
-elif operator == "*":
-    result = num1 * num2
-elif operator == "/":
-    if num2 == 0:
-        print("Error: Division by zero is not allowed.")
-    else:
-        result = num1 / num2
-else:
-    print("Invalid operator. Please choose +, -, *, or /.")
-
-print("Result: ", result)
+# Ask for user input 
+try:
+    print("Calculator: ")
+    num1 = get_number_input("Enter the first number: ")
+    operator = get_operator_input()
+    num2 = get_number_input("Enter the second number: ")
+    result = calculate(num1, operator, num2)
+    print("Result:", result)
+except ZeroDivisionError as e:
+    print(e)
+except KeyboardInterrupt:
+    print("\nCalculation aborted.")
+except Exception as e:
+    print(f"An error occurred: {e}")
